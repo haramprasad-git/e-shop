d382d5be80cf130aa4cdcf2b1341478a13dd824b
Hara M Prasad <haramprasad.09@gmail.com>
2024-01-07 13:50:01 +0530

    Forget to save the file. Recommitting after saving

--- e_shop/customers/models.py
+++ e_shop/customers/models.py
@@ -19,24 +19,24 @@ DISTRICT_LIST = (
         ('Kasargode', 'Kasargode')
     )
 
+
+class Customer(models.Model):
+    core_user = models.OneToOneField(User, related_name='customer', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10, help_text='Use an Indian phone number without country code')
+    signup_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.core_user.username
+
+
 # Data Model to store Customer Address.
 class Address(models.Model):
     district = models.CharField(choice=DISTRICT_LIST, required=True)
     city = models.CharField(max_length=500, required=True)
     street = models.CharField(max_length=500)
     building_no = models.CharField(max_length=20)
+    customer = models.ForeignKey(Customer, related_name='address', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.city
-
-
-
-class Customer(models.Model):
-    core_user = models.OneToOneField(User, related_name='customer', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    address = models.ForeignKey(Address, related_name='customer', on_delete=models.SET_NULL, null=True)
-    phone = models.CharField(max_length=10, help_text='Use an Indian phone number without country code')
-    signup_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self) -> str:
-        return self.core_user.username
